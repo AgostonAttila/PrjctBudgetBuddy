6e62da928e76908e52ec7c70a675e035fe35dea8
AgostonAttila <atesz014@gmail.com>
2024-01-15 22:27:31 +0100

    reorganize client folder

---
+++ Clients/mauiapp/mauiapp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace mauiapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Clients/mauiapp/mauiapp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace mauiapp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
