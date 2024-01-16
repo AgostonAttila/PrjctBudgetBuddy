b7ee61774e101e8ac22255f39e08d2822fb77eaa
AgostonAttila <atesz014@gmail.com>
2024-01-16 21:08:37 +0100

    add some nextjs thing add webapi inbuild auth endpoints + some options  in it

--- Server/WebAPI/WebAPI/Program.cs
+++ Server/WebAPI/WebAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,38 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//builder.Services.AddAuthorization();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+   options => options.UseInMemoryDatabase("AppDb"));
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+    //Lockout
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+    //Password
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 10;
+    options.Password.RequiredUniqueChars = 2;
+    //SignIn 
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+    //User
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
+
 
 var app = builder.Build();
 
@@ -18,7 +54,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.MapIdentityApi<IdentityUser>();
+
+//app.UseAuthorization();
 
 app.MapControllers();
 
