415aee500d56ece28300d5221c4900d4ea4f6af9
AgostonAttila <atesz014@gmail.com>
2024-01-21 23:36:26 +0100

    add swagger to server

--- Clients/reactapp/src/main.tsx
+++ Clients/reactapp/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { Router } from 'react-router-dom';
 import { StoreContext, store } from './app/stores/store.ts';
 import ScrollToTop from './app/layout/ScrollToTop.tsx';
 import {createBrowserHistory} from 'history';
+import App from './app/layout/App.tsx';
 
 export const history = createBrowserHistory();
 
--- Server/WebAPI/WebAPI/Program.cs
+++ Server/WebAPI/WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 using WebAPI.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,14 +11,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddCors();
 
-//builder.Services.AddAuthorization();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    o.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(
    options => options.UseInMemoryDatabase("AppDb"));
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
 {
@@ -50,8 +62,8 @@ var app = builder.Build();
 app.UseCors(x => x
                .AllowAnyMethod()
                .AllowAnyHeader()
-               .SetIsOriginAllowed(origin => true) 
-               .AllowCredentials()); 
+               .SetIsOriginAllowed(origin => true)
+               .AllowCredentials());
 
 
 // Configure the HTTP request pipeline.
